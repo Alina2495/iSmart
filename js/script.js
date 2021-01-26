57c0df8f678aa58419569c4910f13ce4e33ffce4
alinanurislamova <alinanurislamova19@gmail.com>
2021-01-26 22:31:20 +0300

    26 января
    Из дома. Форма поиска в шапке

--- js/script.js
+++ js/script.js
@@ -1,32 +1,54 @@
-const btnsAll = document.querySelectorAll('.list-block-title')
+// search on header
 
-btnsAll.forEach(btn => {
-    btn.addEventListener('click', (event) => {
-        btnsAll.forEach(btn => {
-            btn.classList.remove('active')
-        })
-
-        event.currentTarget.classList.add('active')
-    })
+$('.show-search').on('click', () => {
+    $('.show-search').addClass('hidden');
+    $('.header-menu').addClass('hidden');
+    $('.search').removeClass('hidden');
+    $('.header-search-input').focus();
 })
 
+$('.hide-search').on('click', () => {
+    $('.show-search').removeClass('hidden');
+    $('.header-menu').removeClass('hidden');
+    $('.search').addClass('hidden');
+})
+
+// search on header end
+
+$('.btn-add-item').on('click', () => {
+    $this.text("<b>Some</b> new text.");
+    $('this').addClass('btn-added-item');
+})
+
+// const btnsAll = document.querySelectorAll('.list-block-title')
+
+// btnsAll.forEach(btn => {
+//     btn.addEventListener('click', (event) => {
+//         btnsAll.forEach(btn => {
+//             btn.classList.remove('active')
+//         })
+
+//         event.currentTarget.classList.add('active')
+//     })
+// })
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-    'use strict'
+// (function() {
+//     'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     var forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function(form) {
-            form.addEventListener('submit', function(event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+//     // Loop over them and prevent submission
+//     Array.prototype.slice.call(forms)
+//         .forEach(function(form) {
+//             form.addEventListener('submit', function(event) {
+//                 if (!form.checkValidity()) {
+//                     event.preventDefault()
+//                     event.stopPropagation()
+//                 }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
-})()
+//                 form.classList.add('was-validated')
+//             }, false)
+//         })
+// })()
