a318429611f57e3134cf5dee00942a3c273a8991
ngf93 <nabiullinaguzel93@gmail.com>
2021-02-11 16:43:03 +0300

    slider range

--- js/script.js
+++ js/script.js
@@ -84,6 +84,20 @@ $(".params-title").click(function() {
     $(this).next('.params-list').toggle();
 });
 
+$(document).ready( function() {
+    $("#slider-range").slider({
+      range: true,
+      min: 0,
+      max: 10000,
+      values: [ 2000, 7000 ],
+      slide: function( event, ui ) {
+        $('#min-price').val(ui.values[0]);
+        $('#max-price').val(ui.values[1]);
+      }
+    });
+    $('#min-price').val($( "#slider-range" ).slider( "values", 0 ));
+    $('#max-price').val($( "#slider-range" ).slider( "values", 1 ));
+  });
 
 // Initialize Swiper
 var galleryThumbs = new Swiper('.gallery-thumbs', {
